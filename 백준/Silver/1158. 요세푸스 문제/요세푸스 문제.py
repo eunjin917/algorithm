a71d6a2d3648c773e43b280de49f9edf7d0d81f2
eunGnee <64008627+eunjin917@users.noreply.github.com>
2024-02-19 16:35:38 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+people = [person for person in range(1, N + 1)]
+
+order = []
+next_idx = 0
+for _ in range(N):
+    this_idx = (next_idx + K - 1) % len(people)
+    order.append(people[this_idx])
+    del people[this_idx]
+    next_idx = this_idx
+
+print("<" + ", ".join(str(i) for i in order) + ">")
