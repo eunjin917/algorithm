8f5e73a939ecd81dddea438bbafd26e0b7f1907e
eunGnee <64008627+eunjin917@users.noreply.github.com>
2024-02-21 16:43:45 +0900

    [Silver III] Title: 피보나치 함수, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,10 +1,10 @@
-dp = [0] * 41
+dp = [[0, 0]] * 41
 dp[0], dp[1] = [1, 0], [0, 1]
 
 for i in range(2, 41):
-    dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
-    
-    
+    prev_1, prev_2 = dp[i - 1], dp[i - 2]
+    dp[i] = [prev_1[0] + prev_2[0], prev_1[1] + prev_2[1]]
+
 T = int(input())
 for _ in range(T):
     N = int(input())
