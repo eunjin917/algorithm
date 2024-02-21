f276af7802587dbe3727af7f5b77a13889033014
eunGnee <64008627+eunjin917@users.noreply.github.com>
2024-02-21 13:32:16 +0900

    [Bronze I] Title: 단어 공부, Time: 116 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,18 +1,9 @@
+from collections import Counter
+
 word = input()
+word_dict = Counter(word.upper())
 
-word_dict = {}
-for letter in word.upper():
-    if letter in word_dict:
-        word_dict[letter] += 1
-    else:
-        word_dict[letter] = 1
+max_word_value = max(word_dict.values())
+max_word_keys = [key for key, value in word_dict.items() if value == max_word_value]
 
-word_values = list(word_dict.values())
-max_word_value = max(word_values)
-
-if word_values.count(max_word_value) > 1:
-    print("?")
-else:
-    for key, value in word_dict.items():
-        if value == max_word_value:
-            print(key)
+print(max_word_keys[0] if len(max_word_keys) == 1 else "?")
