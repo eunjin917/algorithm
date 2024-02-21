34e8db27892bb1f58a732791229d7fb86c7690fb
eunGnee <64008627+eunjin917@users.noreply.github.com>
2024-02-21 09:43:42 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+while True:
+    testcase = list(map(int, input().split()))
+    if testcase == [0, 0, 0]:
+        break
+
+    M = max(testcase)
+    if sum([length**2 for length in testcase]) == 2 * M**2:
+        print("right\n")
+    else:
+        print("wrong\n")
