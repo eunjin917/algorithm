512b0da74b4c3fef3413d27a86452418c806ece3
eunGnee <64008627+eunjin917@users.noreply.github.com>
2025-02-05 02:36:25 +0900

    [Bronze II] Title: 피보나치 수, Time: 120 ms, Memory: 15808 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dp; // dp : 피보나치 배열
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // N 입력 : 10
+        int N = Integer.parseInt(br.readLine());
+        dp = new int[N+1];
+        dp[1] = 1;
+
+        // dp[N] 출력 : 55
+        bw.write(fibo(N)+"\n");
+        bw.close();
+    }
+
+    public static int fibo(int N) {
+        // 메모이제이션
+        if (dp[N] != 0 || N == 0) {
+            return dp[N];
+        }
+
+        // 재귀
+        dp[N] = fibo(N-1) + fibo(N-2);
+        return dp[N];
+    }
+}
