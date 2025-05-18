d0f35e23eba8bef8e744a73a0215a54ff22b0aad
eunGnee <64008627+eunjin917@users.noreply.github.com>
2025-05-18 15:24:20 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,12 @@
 # 입력
 N = int(input())
 
-# 계산 : 그리디
-##  5*i + 3*j = N -> 5*0 + 3*3 = 9
-##  i+j가 최소 -> 0+3 = 3
-result = -1
-i = N // 5  # 1 = 9 // 5
-while i >= 0:
-    M = N - 5 * i
-    if M % 3 == 0:
-        j = M // 3
-        result = i + j
-        break
-    i -= 1
+# 계산 : DP
+## 11 = 5 * 1 + 3 * 2
+dp = [-1] * (N + 1)
+for i in range(N // 5 + 1):   # N // 5 = 2 -> 0, 1
+    for j in range(5):  # 0, 1, 2, 3, 4
+        if 5 * i + 3 * j <= N:
+            dp[5 * i + 3 * j] = i + j
 
-# 출력
-print(result)
+print(dp[N])
