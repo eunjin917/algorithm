9d28d320a313e1236ae2df533e628e3580d72b81
eunGnee <64008627+eunjin917@users.noreply.github.com>
2025-06-25 23:58:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 3.14 ms
+메모리: 10.2 MB, 시간: 0.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 25일 23:56:07
+2025년 06월 25일 23:58:25
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,13 +1,4 @@
-from collections import Counter
-
 def solution(nums):
-    N_2 = len(nums)/2
-    counter_nums = Counter(nums)
-    print(counter_nums)
-    
-    answer = 0
-    if len(counter_nums.keys()) >= N_2:
-        answer = N_2
-    else:
-        answer = len(counter_nums.keys())
+    set_nums = set(nums)
+    answer = min(len(set_nums), len(nums)/2)
     return answer
