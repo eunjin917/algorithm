9d70f878186b2ecc5587600b37f67d02598d4989
ChanJin <64008627+eunjin917@users.noreply.github.com>
2025-07-28 10:07:07 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.28 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -5,14 +5,13 @@ import java.lang.*;
 class Solution {
     
     public int dfs(int startNode, List<List<Integer>> tree, boolean[] visited) {
-        int count = 0;
+        int count = 1;
+        visited[startNode] = true;
+        
         for (int node : tree.get(startNode)) {
-            if (visited[node]) {
-                continue;
+            if (!visited[node]) {
+                count += dfs(node, tree, visited);
             }
-            visited[node] = true;
-            count++;
-            count += dfs(node, tree, visited);
         }
         return count;
     }
@@ -27,21 +26,15 @@ class Solution {
             tree.get(wire[1]).add(wire[0]);
         }
         
-        // for (int i = 1; i < tree.size(); i++) {
-        //     System.out.println(i + " → " + tree.get(i));
-        // }
-        
         int minDiff = Integer.MAX_VALUE;
         for (int[] wire : wires) {
             int[] count = new int[2];
-            for (int k=0; k<=1; k++) { // 수정
+            for (int k=0; k<=1; k++) {
                 boolean visited[] = new boolean[n+1];
-                visited[wire[k]] = true;
                 visited[wire[(k+1)%2]] = true;
                 count[k] = dfs(wire[k], tree, visited);
             }
-            int diff = count[1]-count[0];
-            diff = diff > 0 ? diff : -diff;
+            int diff = Math.abs(count[1]-count[0]);
             minDiff = Math.min(diff, minDiff);
         }
         
