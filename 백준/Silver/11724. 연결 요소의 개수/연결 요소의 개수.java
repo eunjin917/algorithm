1684865260713b040f449ada3ea64400d9c07f47
ChanJin <64008627+eunjin917@users.noreply.github.com>
2025-08-11 13:41:31 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 572 ms, Memory: 141928 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,54 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static List<Integer>[] adj = new ArrayList[1002];
-    static boolean[] visited = new boolean[1002];
 
-    static void dfs(int here) {
-
-        for (int i = 0; i < adj[here].size(); i++) {
-            int there = adj[here].get(i);
-            if (!visited[there]) {
-                visited[there] = true;
-                dfs(there);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i = 1; i <= n; i++) {
-            adj[i] = new ArrayList<>();
-        }
-
-        int a, b;
-        for (int i = 1; i <= m; i++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            adj[a].add(b);
-            adj[b].add(a);
-        }
-        int ret = 0;
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(i);
-                ret++;
-            }
-        }
-        bw.write(ret + "");
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 입력
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		// 맵 구성
+		@SuppressWarnings("unchecked")
+		List<Integer>[] map = (ArrayList<Integer>[]) new ArrayList[N + 1];
+		for (int i=1; i<=N; i++) {
+			map[i] = new ArrayList<>();
+		}
+		
+		for (int j=1; j<=M; j++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			map[u].add(v);
+            map[v].add(u);
+		}
+		
+		// 네트워크 그룹 찾기
+		int groupCount = 0;
+		boolean[] visited = new boolean[N+1];
+		for (int i=1; i<=N; i++) {
+			if (!visited[i]) {
+				groupCount += BFS(i, map, visited);
+				
+			}
+		}
+		
+		// 출력
+		System.out.println(groupCount);
+	}
+	
+	public static int BFS(int start, List<Integer>[] map, boolean[] visited) {
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		q.offer(start);
+		visited[start] = true;
+		
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			for (int next : map[cur]) {
+				if (!visited[next]) {
+					q.offer(next);
+					visited[next] = true;
+				}
+			}
+		}
+		
+		return 1;
+	}
 }
